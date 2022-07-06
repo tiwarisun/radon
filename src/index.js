9c16162957c9ccd595014a6c091541f85a053cc6
debobratade-github <debobratade@gmail.com>
2022-07-06 11:53:12 +0530

    structure

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const route = require('./routes/routes.js');
+const mongoose  = require('mongoose');
+const app = express();
+
+app.use(bodyParser.json());
+
+
+
+mongoose.connect("mongodb+srv://Raichu:Rishi1234@cluster0.xw5ct.mongodb.net/group20Database?retryWrites=true&w=majority", {
+    useNewUrlParser: true
+})
+.then( () => console.log("MongoDb is connected"))
+.catch ( err => console.log(err) )
+
+
+app.use('/', route);
+
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Express app running on port ' + (process.env.PORT || 3000))
+});
