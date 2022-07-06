28b22b3033b73ca4af4bd4801e3593cbda651efc
tiwarisun <suniltiwari460@gmail.com>
2022-07-06 12:40:25 +0530

    project/booksManagementGroup20

---
+++ src/models/userModel.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+const userSchema = new mongoose.Schema({
+
+    
+        title: {
+            type: String, 
+            require : true,
+            enum : ['Mr', 'Mrs', 'Miss']
+        },
+
+
+        name: {
+
+            type : String,
+            require: true
+        },
+
+
+        phone: {
+            type: String, 
+            require: true,
+            unique: true
+        },
+        email: {
+            type : String,
+             required : true,
+             unique : true,
+             validate : {
+             validator : validator.isEmail,
+            message : "Not a valid email"}
+        }, 
+
+
+        password: {
+            type : String,
+            required : true,
+             
+          },
+
+
+
+        address: {
+          street:{ 
+          type: String
+         },
+          city: {
+          type: String
+          },
+          pincode: {
+          type: String
+         }
+        },
+
+
+      timestamps: true 
+    
+})
+
+module.exports = mongoose.model('User', userSchema)
